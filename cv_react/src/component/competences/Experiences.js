24003a3c4542b978d15f4b5addc3eb86fe5d1d1b
Hugo Rodrigues <hugo38.rodrigues@gmail.com>
2022-03-23 15:34:52 +0100

    modification classname

--- cv_react/src/component/competences/Experiences.js
+++ cv_react/src/component/competences/Experiences.js
@@ -4,12 +4,12 @@ const Experiences = () => {
     return (
         <div className="excperience">
             <h3> Experiences</h3>
-            <div className='exp1'>
+            <div className='exp-1'>
                 <h4>Alternance développeur Front/Back</h4>
                 <h5>Tecknowmetrix, Voiron(38) Sept.2021-Sept.2022</h5>
                 <p>Alternance de développeur front/back au sein de l'équipe R&D de Tecknowmetrix</p>
             </div>
-            <div className='exp2'>
+            <div className='exp-2'>
                 <h4>Stagiaire en électronique embarquée</h4>
                 <h5>Systel Electronique, Montchanin (71) Avr.2021 – Juin.2021 </h5>
                 <p>Stage de fin d'étude en DUT, au poste de technicien en
