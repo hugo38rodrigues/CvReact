eef48cacbec991de2a4f0e8ba2e15c404c442337
Hugo Rodrigues <hugo38.rodrigues@gmail.com>
2022-03-23 14:52:04 +0100

    remplissage du composant expérience

--- cv_react/src/component/competences/Experiences.js
+++ cv_react/src/component/competences/Experiences.js
@@ -2,8 +2,22 @@ import React from 'react';
 
 const Experiences = () => {
     return (
-        <div>
-            Experiences
+        <div className="excperience">
+            <h3> Experiences</h3>
+            <div className='exp1'>
+                <h4>Alternance développeur Front/Back</h4>
+                <h5>Tecknowmetrix, Voiron(38) Sept.2021-Sept.2022</h5>
+                <p>Alternance de développeur front/back au sein de l'équipe R&D de Tecknowmetrix</p>
+            </div>
+            <div className='exp2'>
+                <h4>Stagiaire en électronique embarquée</h4>
+                <h5>Systel Electronique, Montchanin (71) Avr.2021 – Juin.2021 </h5>
+                <p>Stage de fin d'étude en DUT, au poste de technicien en
+                    électronique dans le bureau R&D. Prototypage et conception
+                    d'une carte électronique pour la recharge par induction d'une
+                    télécommande pilotant des vannes.</p>
+            </div>
+
         </div>
     );
 };
