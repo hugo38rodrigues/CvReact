43660c6883ced2e4417e9c6f1202932ac3780235
Hugo Rodrigues <hugo38.rodrigues@gmail.com>
2022-05-15 20:31:26 +0200

    correction du texcte

--- cv_react/src/component/competences/Experiences.js
+++ cv_react/src/component/competences/Experiences.js
@@ -7,11 +7,11 @@ const Experiences = () => {
       <div className="exp-1">
         <h4>Alternance assistant chef de projet / DevOps</h4>
         <h5>Tecknowmetrix, Voiron(38) Sept.2021-Sept.2022</h5>
-        <ul>
-          <li>Mise en place et amélioration continu de Jira au sein de TKM </li>
-          <li>Former l'équipe R&D sur git pour l'utilisation de bitbucket</li>
-          <li>Création d'un processus de cycle de vie d'un logiciel </li>
-        </ul>
+        <p>
+          J'ai mis en place Jira dans l'entreprise TKM aisni que Bitbucket. J'ai
+          aussi former l'équipe à l'utilisation de git. Enfin j'ai crée un
+          processus de cycle de vie d'un logiciel.
+        </p>
       </div>
       <div className="exp-2">
         <h4>Stagiaire en électronique embarquée</h4>
