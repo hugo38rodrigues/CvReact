22a69a83ea54338f296ee9935a03f51aede33c1b
Hugo Rodrigues <hugo38.rodrigues@gmail.com>
2022-05-15 17:09:03 +0200

    add expérience

--- cv_react/src/component/competences/AutresCompetences.js
+++ cv_react/src/component/competences/AutresCompetences.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
 
 const AutresCompetences = () => {
-    return (
-        <div>
-            AutresCompetences
+  return (
+    <div>
+      <div className="otherSkills">
+        <h3>Autre compéternces</h3>
+        <div className="list">
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default AutresCompetences;
