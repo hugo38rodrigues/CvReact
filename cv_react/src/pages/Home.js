27830d7422095b392387e58aaba8dd2d50061e44
Hugo Rodrigues <hugo38.rodrigues@gmail.com>
2022-05-15 20:32:51 +0200

    mise a jour de la syntaxe et changement du chemin pour le cv en pdf

--- cv_react/src/pages/Home.js
+++ cv_react/src/pages/Home.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import Navigation from '../component/Navigation';
+import React from "react";
+import Navigation from "../component/Navigation";
 const Home = () => {
-    return (
-        <div className='home'>
-            <Navigation />
-            <div className='homeContent'>
-                <div className='content'>
-                    <h1> Hugo Rodrigues</h1>
-                    <h2>Développeur Front-end</h2>
-                    <div className='pdf'>
-                        <a href="./media/CV.pdf" target="_blank">Télécharge CV</a>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="home">
+      <Navigation />
+      <div className="homeContent">
+        <div className="content">
+          <h1> Hugo Rodrigues</h1>
+          <h2>Développeur Front-end</h2>
+          <div className="pdf">
+            <a href="../asset/CV.pdf" target="_blank">
+              Télécharge CV
+            </a>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- cv_react/src/pages/NotFound.js
+++ cv_react/src/pages/NotFound.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
 const NotFound = () => {
-    return (
-        <div className='notFound'>
-            <div className='notFoundContent'>
-                <h3> Désoler cette page n'existe pas</h3>
-                <NavLink exact to ="/"> Acceuil</NavLink>
-            </div>
-        </div>
-    );
+  return (
+    <div className="notFound">
+      <div className="notFoundContent">
+        <h3> Désoler cette page n'existe pas</h3>
+        <NavLink exact to="/">
+          <FontAwesomeIcon icon={faHome} /> <span>Acceuil</span>
+        </NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
