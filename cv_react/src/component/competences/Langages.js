10b00bedf1a73ab0f377edf19c6e535a52d3ee4e
Hugo Rodrigues <hugo38.rodrigues@gmail.com>
2022-05-15 19:47:03 +0200

    modification des données

--- cv_react/src/component/competences/Langages.js
+++ cv_react/src/component/competences/Langages.js
@@ -1,37 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import ProgressBar from "./ProgressBar";
 
 export default class Langages extends Component {
-    state={
-        langages:[
-            {id: 1, value:"Javascript", xp: 0.7},
-            {id: 2, value:"HTML5/CSS3", xp: 0.7},
-            {id: 3, value: "Python", xp: 0.5},   
-            {id: 4, value: "C++", xp: 1}
-        ],
-        framworks:[
-            {id: 1, value: "React",xp: 0.2}
-
-        ]
-    }
+  state = {
+    langages: [
+      { id: 1, value: "Javascript", xp: 0.9 },
+      { id: 2, value: "HTML5/CSS3", xp: 1 },
+      { id: 3, value: "Python", xp: 0.5 },
+      { id: 4, value: "C++", xp: 1 },
+    ],
+    framworks: [{ id: 1, value: "React", xp: 1 }],
+  };
   render() {
-      let{langages,framworks}=this.state;
+    let { langages, framworks } = this.state;
 
-    return(
-        <div className='"languagesFrameworks'>
-            <ProgressBar 
-                langages={langages}
-                className="langagesDisplay"
-                title="langages"
-            />
-            <ProgressBar 
-                title="framworks & bibliothèque"
-                langages={framworks}
-                className="framworksDisplay"
-                
-            />
-
-        </div>
-    ) 
+    return (
+      <div className="languagesFrameworks">
+        <ProgressBar
+          langages={langages}
+          className="langagesDisplay"
+          title="langages"
+        />
+        <ProgressBar
+          title="framworks & bibliothèque"
+          langages={framworks}
+          className="framworksDisplay"
+        />
+      </div>
+    );
   }
 }
--- cv_react/src/component/competences/ProgressBar.js
+++ cv_react/src/component/competences/ProgressBar.js
@@ -1,38 +1,30 @@
-import React from 'react';
+import React from "react";
 
 const ProgressBar = (props) => {
-    
-    return (
-        <div className={props.className}>
-            <h3>{props.title}</h3>
-            <div className="years">
-                <span>Années d'expériences</span>
-                <span>2 mois</span>
-                <span>1 ans</span>
+  return (
+    <div className={props.className}>
+      <h3>{props.title}</h3>
+      <div className="years">
+        <span>Années d'expériences</span>
+        <span>2 mois</span>
+        <span>1 ans</span>
+      </div>
+
+      <div>
+        {props.langages.map((item) => {
+          let xpYears = 1;
+          let progressBar = (item.xp / xpYears) * 100 + "%";
+
+          return (
+            <div key={item.id} className="languagesList">
+              <li>{item.value}</li>
+              <div className="progressBar" style={{ width: progressBar }}></div>
             </div>
-
-            <div>
-                {
-                    props.langages.map((item) => {
-                        let xpYears = 1;
-                        let progressBar = item.xp / xpYears * 100 + "%";
-
-                        return (
-                            <div key={item.id} className="languagesList">
-                                <li>{item.value}</li>
-                                <div className="progressBar" style={{width:progressBar }}></div>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-
-
-
-
-
-        </div>
-    );
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default ProgressBar;
