8b95f04f64924c60a4ec8f1387d87680cad9800e
Hugo Rodrigues <hugo38.rodrigues@gmail.com>
2022-05-19 16:21:57 +0200

    création des pages project pour le portfolio

--- cv_react/src/pages/Portefolio.js
+++ cv_react/src/pages/Portefolio.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import Navigation from '../component/Navigation';
+import React from "react";
+import Navigation from "../component/Navigation";
+import ProjectList from "../component/portfolio/ProjectList";
 
 const Portefolio = () => {
-    return (
-        <div className='Portefolio'>
-            <Navigation />
-        </div>
-    );
+  return (
+    <div className="Portefolio">
+      <Navigation />
+      <ProjectList />
+    </div>
+  );
 };
 
 export default Portefolio;
