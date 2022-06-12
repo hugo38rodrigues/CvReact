83534b58c7e0c259c20c5214f1a9fb3692c120a8
Hugo Rodrigues <hugo38.rodrigues@gmail.com>
2022-06-12 19:40:51 +0200

    création de la page portfolio

--- cv_react/src/component/portfolio/ProjectList.js
+++ cv_react/src/component/portfolio/ProjectList.js
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
+import { portfolioData } from "../../data/portfolioDatas";
+import Project from "./Project";
 
-const ProjectList = () => {
-  return <div>project</div>;
-};
-
-export default ProjectList;
+export default class ProjectList extends Component {
+  state = {
+    project: portfolioData,
+    radios: [
+      { id: 1, value: "React" },
+      { id: 2, value: "CSS" },
+      { id: 3, value: "PHP" },
+      { id: 4, value: "JAVA" },
+    ],
+    selectedRadio: "React",
+  };
+  handleRadio = (event) => {
+    let radio = event.tagert.value;
+    this.setState({ selectedRadio: radio });
+  };
+  render() {
+    let { project, radios, selectedRadio } = this.state;
+    return (
+      <div className="portfolioContent">
+        <ul className="radioDisplay">
+          {radios.map((radio) => {
+            return (
+              <li key={radio.id}>
+                <input
+                  type="radio"
+                  name="radio"
+                  checked={radio.value === selectedRadio}
+                  value={radio.value}
+                  id={radio.value}
+                  onChange={this.handleRadio}
+                />
+                <label htmlFor={radio.value}>{radio.value}</label>
+              </li>
+            );
+          })}
+        </ul>
+        <div className="projects">
+          {project
+            .filter((item) => item.languages.includes(selectedRadio))
+            .map((item) => {
+              return <Project item={item} key={item.id} />;
+            })}
+        </div>
+      </div>
+    );
+  }
+}
---
+++ cv_react/src/data/portfolioDatas.js
@@ -0,0 +1,28 @@
+export const portfolioData = [
+  {
+    id: 1,
+    name: "application 2R",
+    languages: ["React", "CSS", "PHP"],
+    languagesIcons: [
+      "fa-brands fa-react",
+      "fa-brands fa-php",
+      "fa-brands fa-css3-alt",
+    ],
+    source: "https://github.com/hugo38rodrigues/projet_2r",
+    info: "Le projet 2r est un projet réalisé dans le cadre de ma licence professionnelle, le but de ce projet était de réaliser un site internet de type réseau social. Le projet respecte une conception de type MVC avec une API en PHP, une BDD en MYSQL et un front-end en React.",
+    picture: "./media/projet1.png",
+  },
+  {
+    id: 2,
+    name: "Gestion de pot de peinture",
+    languages: ["React", "CSS", "JAVA"],
+    languagesIcons: [
+      "fa-brands fa-react",
+      "fa-brands fa-css3-alt",
+      "fa-brands fa-java",
+    ],
+    source: "https://github.com/hugo38rodrigues/AppPeinture",
+    info: "Le but du projet App Peinture est de créer une application web qui permet de gérer un stock de pots de peinture et de maquettes. Il est possible de créer des pots de peinture et des modèles pour les ajouter au stock, de commander les pots manquants dans le stock par une notification par email.",
+    picture: "./media/projet2.png",
+  },
+];
