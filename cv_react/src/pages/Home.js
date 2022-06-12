68823a7925b727a1f63e1da86544763b8dd16092
Hugo Rodrigues <hugo38.rodrigues@gmail.com>
2022-06-12 19:41:42 +0200

    changement d'emplacement du CV.pdf

--- cv_react/src/pages/Home.js
+++ cv_react/src/pages/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
           <h1> Hugo Rodrigues</h1>
           <h2>Développeur Front-end</h2>
           <div className="pdf">
-            <a href="../asset/CV_entreprise.pdf" download>
+            <a href="./CV_entreprise.pdf" download="CV_entreprise.pdf">
               Télécharger le CV en pdf
             </a>
           </div>
