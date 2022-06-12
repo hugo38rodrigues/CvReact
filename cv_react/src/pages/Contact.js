d9f28ca031dbcf582c8c92e6f6e17fbeb0cab324
Hugo Rodrigues <hugo38.rodrigues@gmail.com>
2022-06-12 19:41:10 +0200

    création de la page contact

--- cv_react/src/pages/Contact.js
+++ cv_react/src/pages/Contact.js
@@ -1,12 +1,70 @@
-import React from 'react';
-import Navigation from '../component/Navigation';
-
+import React from "react";
+import Navigation from "../component/Navigation";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 const Contact = () => {
-    return (
-        <div className='Contact'>
-            <Navigation />
+  return (
+    <div className="contact">
+      <Navigation />
+      <div className="contactContent">
+        <div className="header"></div>
+        <div className="contactBox">
+          <h1>Contactez-moi</h1>
+          <ul>
+            <li>
+              <i className="fas fa-map-marker-alt"></i>
+              <span>Grenoble</span>
+            </li>
+            <li>
+              <i className="fas fa-mobile-alt"></i>
+              <CopyToClipboard text="0604478459">
+                <span
+                  className="clickInput"
+                  onClick={() => {
+                    alert("Téléphone copié !");
+                  }}
+                >
+                  06 04 47 84 59{" "}
+                </span>
+              </CopyToClipboard>
+            </li>
+            <li>
+              <i className="far fa-envelope"></i>
+              <CopyToClipboard text="hugo38.rodrigues@gmail.com">
+                <span
+                  className="clickInput"
+                  onClick={() => {
+                    alert("E-mail copié !");
+                  }}
+                >
+                  hugo38.rodrigues@gmail.com
+                </span>
+              </CopyToClipboard>
+            </li>
+          </ul>
         </div>
-    );
+        <div className="socialNetwork">
+          <ul>
+            <a
+              href="https://www.linkedin.com/in/rodrigues-hugo/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h4>Linkedin</h4>
+              <i className="fab fa-linkedin"></i>
+            </a>
+            <a
+              href="https://github.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h4>Github</h4>
+              <i className="fab fa-github"></i>
+            </a>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
